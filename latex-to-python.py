fe48e4925454fd284113119fe512176c125062fa
dv <wdv4758h@gmail.com>
2014-01-29 10:26:03 +0800

    change class to be all functions

--- latex-to-python.py
+++ latex-to-python.py
@@ -1,40 +1,35 @@
 #!/usr/bin/python3
 
 import re
-from functools import reduce
+from functools import reduce, partial
 
-class latex_to_python():
+def pattern_gen(s):
+    """
+    generate : r'\syntax{}' + r'%s'
+    """
 
+    #+?    : non-greedy of +
+    #{.*}* : avoid match in the main {}
+
+    return (r'%s{(.+?({.*})*)}%s' % (s[0], s[2]), s[1])
+
+def replace(string, s_tuple):
+    """
+    string  : string to be used in replacement
+    s_tuple : (pattern, repl)
+    """
+    return re.sub(s_tuple[0], s_tuple[1], string)
+
+def convert(patterns, expression):
+    return reduce(replace, patterns, expression)
+
+def main():
     #(syntax, repl, addtional regex pattern)
     syntax = [(r'\\frac', '\g<1>/\g<3>', '{(.+?({.*})*)}'),
               ('\^', '**\g<1>', ''),]
-    patterns = []
+    patterns = tuple(map(pattern_gen, syntax))
 
-    def __init__(self):
-        self.patterns = tuple(map(self.pattern_gen, self.syntax))
-
-    def pattern_gen(self, s):
-        """
-        generate : r'\syntax{}' + r'%s'
-        """
-
-        #+?    : non-greedy of +
-        #{.*}* : avoid match in the main {}
-
-        return (r'%s{(.+?({.*})*)}%s' % (s[0], s[2]), s[1])
-
-    def replace(self, string, s_tuple):
-        """
-        string  : string to be used in replacement
-        s_tuple : (pattern, repl)
-        """
-        return re.sub(s_tuple[0], s_tuple[1], string)
-
-    def convert(self, expression):
-        return reduce(self.replace, self.patterns, expression)
-
-if __name__ == '__main__':
-    to_python = latex_to_python().convert
+    to_python = partial(convert, patterns)
     while(True):
         expression = input("Please input your latex expression: ")
         print(to_python(expression))
@@ -42,3 +37,6 @@ if __name__ == '__main__':
             print(eval(to_python(expression)))
         except:
             print("expression not evaluatable")
+
+if __name__ == '__main__':
+    main()
